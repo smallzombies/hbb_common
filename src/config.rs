e59b96a382b50fcd1eebdd131ac94cbba70a0c78
smallzombies <125415937+smallzombies@users.noreply.github.com>
2025-09-07 06:32:29 +0800

    Update config.rs
    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xuzl.dpdns.org"];
+pub const RS_PUB_KEY: &str = "mC8CcPWAsO1UAGRmvZIFHIlrUHkpqP+UN3+Nva+3qi8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
